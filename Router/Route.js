e7f0fb29aaeba89e31823468220d5ec9325e2275
Mourtalha <mourtalha@hotmail.fr>
2024-04-23 11:28:02 +0200

    Ajout systeme de routage

---
+++ Router/Route.js
@@ -0,0 +1,15 @@
+export default class Route {
+  2;
+  constructor(url, title, pathHtml, pathJS = "") {
+    3;
+    this.url = url;
+    4;
+    this.title = title;
+    5;
+    this.pathHtml = pathHtml;
+    6;
+    this.pathJS = pathJS;
+    7;
+  }
+  8;
+}
---
+++ Router/allRoutes.js
@@ -0,0 +1,11 @@
+import Route from "./Route.js";
+2;
+3;
+//Définir ici vos routes
+4;
+export const allRoutes = [new Route("/", "Accueil", "/pages/home.html")];
+6;
+7;
+//Le titre s'affiche comme ceci : Route.titre - websitename
+8;
+export const websiteName = "Quai Antique";
