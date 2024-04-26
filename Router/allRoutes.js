d91247d972c1180702286777fd1a6608ea8e7938
Mourtalha <mourtalha@hotmail.fr>
2024-04-27 01:30:31 +0200

    Verification champ requis Inscription

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -8,7 +8,12 @@ export const allRoutes = [
   new Route("/galerie", "la galerie", "/pages/galerie.html"),
   new Route("/menu", "le Menu", "/pages/menu.html"),
   new Route("/signin", "Connexion", "/pages/auth/signin.html"),
-  new Route("/signup", "Inscription", "/pages/auth/signup.html"),
+  new Route(
+    "/signup",
+    "Inscription",
+    "/pages/auth/signup.html",
+    "/js/auth/signup.js"
+  ),
   new Route("/account", "Mon compte", "/pages/auth/account.html"),
   new Route(
     "/editPassword",
